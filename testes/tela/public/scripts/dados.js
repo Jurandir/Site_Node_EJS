ed08122e211ed33b4b08ba63680d124b780b645c
Jurandir Brito Ferreira Junior <jurandir.junior@termaco.com.br>
2021-03-08 08:58:31 -0300

    Testes Telas

---
+++ testes/tela/public/scripts/dados.js
@@ -0,0 +1,70 @@
+const API_DADOS = {
+	"numero": 3138594,
+	"dataEmissao": "2021-02-01T00:00:00.000Z",
+	"prevEntrega": "2021-02-16T00:00:00.000Z",
+	"tipoPesoM3": "VOL",
+	"pesoM3": 8,
+	"valorMercadoria": 54700.96,
+	"valorFrete": 2896.54,
+	"chave": "35210211552312000710570010031385941571279840",
+	"origemPrestacao": {
+		"nome": "LOUVEIRA",
+		"uf": "SP",
+		"ibge": "3527306"
+	},
+	"notaFiscal": {
+		"numero": "56885",
+		"serie": "2",
+		"dataEmissao": "2021-01-30T00:00:00.000Z",
+		"valor": 54700.96,
+		"chaveNFe": "35210116851732000206550020000568851807615768"
+	},
+	"unidadeDestino": {
+		"sigla": "CAR",
+		"nome": "TERMACO CARUARU",
+		"endereco": "ESTRADA ALTO DO MOURA",
+		"numero": "582",
+		"bairro": "ALTO DO MOURA",
+		"cidade": {
+			"ibge": "2604106",
+			"nome": "CARUARU",
+			"uf": "PE"
+		}
+	},
+	"ocorrencias": [
+		{
+			"codigoInterno": 0,
+			"codigoProceda": 0,
+			"descricaoOcorrencia": "PROCESSO DE TRANSPORTE JA INICIADO",
+			"dataRegistro": "2021-02-01T15:21:19.000Z"
+		},
+		{
+			"codigoInterno": 98,
+			"codigoProceda": 98,
+			"descricaoOcorrencia": "CHEGADA NA CIDADE OU FILIAL DE DESTINO",
+			"dataRegistro": "2021-02-05T18:00:00.000Z"
+		},
+		{
+			"codigoInterno": 1,
+			"codigoProceda": 1,
+			"descricaoOcorrencia": "ENTREGA REALIZADA NORMALMENTE",
+			"dataRegistro": "2021-02-08T17:39:12.000Z"
+		}
+	],
+	"destinoPrestacao": {
+		"nome": "INTERIOR PERNAMBUCO",
+		"uf": "PE",
+		"ibge": "1111111"
+	},
+	"localEntrega": {
+		"nome": "MARIA DE OLIVEIRA ALMEIDA",
+		"endereco": "RUA MAJOR NEGRINHO",
+		"numero": "23",
+		"bairro": "BAIRRO NOVO",
+		"cidade": {
+			"nome": "SANTA CRUZ DO CAPIBARIBE",
+			"uf": "PE",
+			"ibge": "2612505"
+		}
+	}
+}
